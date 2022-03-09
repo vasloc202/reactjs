6215f2b5842944b08536fc8fe89445d230da3aee
vasloc202 <nvloc2510z@gmail.com>
2022-03-09 19:18:29 +0700

    Xóa hết làm lại

---
+++ index.ts
@@ -0,0 +1,27 @@
+export{};
+type Product = {
+    id: number,
+    name: string,
+    price: number
+}
+type Product2 = {
+    id: string,
+    name: string,
+    status: boolean
+}
+type MergeType = Product | Product2;
+
+const firstName:string = "Abc";
+const firstNumber:number = 123;
+const trueFalse:boolean = true;
+const stringArr: string[] = ["a","b","c"];
+const objArr: MergeType[] = [
+    { id: 1, name: "Abc", price: 12312312 },
+    { id: "123", name: "Abc", status: true }
+];
+
+function sumNumber(a:number,b:number) {
+    return (a*b)-(a+b);
+}
+console.log(sumNumber(3,4));
+ 
