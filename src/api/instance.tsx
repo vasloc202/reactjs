31ecae7d88c473aaf4587aeee09804e8d674070f
vasloc202 <nvloc2510z@gmail.com>
2022-04-15 08:23:12 +0700

    Feat : Update

--- src/api/instance.tsx
+++ src/api/instance.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:2222/api",
+  baseURL: "http://localhost:3333/api",
   headers: {
     "Content-Type": "application/json",
   }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -13,6 +13,7 @@ type HomePageProps = {
 const HomePage = (props: HomePageProps) => {
   const [products, setProducts] = useState<ProductType[]>([]);
   const [categories, setCategories] = useState<Categories[]>([]);
+  const [category, setCategory] = useState<ProductType[]>([]);
   const [page, setPage] = useState<number>(1);
   const [perPage, setPerPage] = useState<number>(8);
   const [count, setCount] = useState<number>();
@@ -26,7 +27,7 @@ const HomePage = (props: HomePageProps) => {
     };
     getProducts();
   }, [page]);
-  // category
+  // list category
   const getCategories = async () => {
     const { data } = await getAll();
     setCategories(data);
@@ -37,9 +38,9 @@ const HomePage = (props: HomePageProps) => {
   // list product in category
   const clickCategory = async (id: any) => {
     const { data } = await read(id)
-    setCategories(data);
+    setCategory(data.products);
+    setProducts([]);
   }
-
   return (
     <div>
       <div>
@@ -65,6 +66,39 @@ const HomePage = (props: HomePageProps) => {
       </div>
       <div className="container grid grid-cols-4 px-32">
         {products.map((item, index) => (
+          <div className="" key={index}>
+            <div className="max-w-2xl mx-auto lg:py-4 sm:px-6 lg:max-w-7xl lg:px-8 text-black">
+              <div className="grid grid-cols-4 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                <div className="group w-64">
+                  <div className="w-64 min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+                    <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men's Basic Tee in black." className="w-full h-full object-center object-cover lg:w-full lg:h-full" />
+                  </div>
+                  <div className="w-64 mt-4 block text-center">
+                    <h3 className="text-2xl text-gray-700 ">
+                      <Link to={`/products/${item._id}`}>
+                        <span aria-hidden="true" className="inset-0" />
+                        {item.name}
+                      </Link>
+                    </h3>
+                  </div>
+                  <div className="w-64 mt-2 text-center">
+                    <p className="text-lg font-medium text-red-500">
+                      {new Intl.NumberFormat("VND", { style: "currency", currency: "VND" }).format(item.price)}
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <Link to={`/products/${item._id}`} className="mt-4 text-sm text-gray-500">
+                  Chi tiết
+                </Link>
+              </div>
+
+            </div>
+          </div>
+        ))
+        }
+        {category.map((item, index) => (
           <div className="" key={index}>
             <div className="max-w-2xl mx-auto lg:py-4 sm:px-6 lg:max-w-7xl lg:px-8 text-black">
               <div className="grid grid-cols-4 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
